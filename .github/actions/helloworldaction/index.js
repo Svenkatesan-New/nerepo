2146168bbc92d1389b85d8201ddc3868500ee6ea
Svenkatesan-New <svenkatesan17401@gmail.com>
2025-03-25 19:40:01 +0530

    c

--- .github/actions/helloworldaction/index.js
+++ .github/actions/helloworldaction/index.js
@@ -1,11 +1,11 @@
 // .github/actions/hello-world-action/index.js
-// const core = require('@actions/core');
+const core = require('@actions/core');
 
-// try {
-//   // Input parameter for custom message
-//   const message = core.getInput('message');
-//   console.log(`Hello, ${message}!`);
-// } catch (error) {
-//   core.setFailed(`Action failed with error: ${error.message}`);
-// }
-console.log("hello")
+try {
+  // Input parameter for custom message
+  const message = core.getInput('message');
+  console.log(`Hello, ${message}!`);
+} catch (error) {
+  core.setFailed(`Action failed with error: ${error.message}`);
+}
+// console.log("hello")
