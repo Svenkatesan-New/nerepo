cf1c294ddd43f759bb9a2bb2273782922682d829
Svenkatesan-New <svenkatesan17401@gmail.com>
2025-03-25 22:48:54 +0530

    co

--- main.tf
+++ main.tf
@@ -9,7 +9,7 @@ provider "azurerm" {
 
 # Create Resource Group
 resource "azurerm_resource_group" "example" {
-  name     = "example-resources1"
+  name     = "example-resources12"
   location = "East US"
 }
 
@@ -36,7 +36,7 @@ resource "azurerm_app_service_plan" "example" {
 
 # Create Function App
 resource "azurerm_function_app" "example" {
-  name                      = "examplefunctionapp"
+  name                      = "examplefunctionappthisisademoappfordemo"
   location                  = azurerm_resource_group.example.location
   resource_group_name       = azurerm_resource_group.example.name
   app_service_plan_id       = azurerm_app_service_plan.example.id
